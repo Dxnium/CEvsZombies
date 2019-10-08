432e23059fe4442695bd78252d66032fc0cb3558
Dxnium <38996231+Dxnium@users.noreply.github.com>
2019-10-08 17:16:51 -0600

    intial commit
    Project created

---
+++ cmake-build-debug/CMakeFiles/CEvsZombies.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CEvsZombies"
+  "CEvsZombies.pdb"
+  "CMakeFiles/CEvsZombies.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CEvsZombies.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
